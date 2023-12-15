c7e2f05bedb2387b3c5093c111881fc086976d6b
Kevin_in_ASUS <kevinjyh@gmail.com>
2023-12-15 21:37:37 +0800

    空白鍵讓鳥飛起來了！

--- Assets/NewBehaveiousScript.cs
+++ Assets/NewBehaveiousScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class NewBehaveiousScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,8 @@ public class NewBehaveiousScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) == true){
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
     }
 }
